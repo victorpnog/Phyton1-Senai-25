51266b0c98cd34232995aa8cc8303a7a4dfed24b
Victor Pimentel Nogueira <107135601+victorpnog@users.noreply.github.com>
2025-02-06 19:53:44 -0300

    Add files via upload

--- 013.py
+++ 013.py
@@ -5,10 +5,8 @@
 
 #leitura de dados
 frase = input('Escreva uma frase: ')
-primeiro_a = frase.find ('a') + 1
-ultimo_a = frase.rfind('a') + 1
 
 #processamento de dados
-print(f'\nEssa frase tem {frase.count('a')} "a"')
-print(f'\nO primeiro "a" aparece na posição {primeiro_a}')
-print(f'\nO último "a" aparece na posição {ultimo_a}')
+print(f'\nEssa frase tem {frase.count('A')}' 
+      f'\nO primeiro "a" aparece na posição {frase.find ('A') + 1 }'
+      f'\nO último "a" aparece na posição {frase.rfind('A') + 1 }')
