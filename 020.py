29b58fc8a1a45fe5f1a4d8a7b41ed96d1ea650b3
Victor Pimentel Nogueira <107135601+victorpnog@users.noreply.github.com>
2025-02-07 14:53:18 -0300

    Add files via upload

--- 020.py
+++ 020.py
@@ -3,15 +3,22 @@
 idade = int(input('Digite sua idade: '))
 
 if idade >= 16 and idade <= 69:
-    peso = float(input('Digte seu peso: '))
+    peso = float(input('\nDigte seu peso: '))
     if peso >= 50:
-        alcool = input('Você ingeriu álcool nas últimas 12 horas? Escreva SIM ou NÃO: ').lower().strip()[0]
+        alcool = input('\nVocê ingeriu álcool nas últimas 12 horas? Escreva SIM ou NÃO: ').lower().strip()[0]
         if alcool == 'n':
-            print('Não pode doar')
+            sono = input('\nVocê dormiu 6h ou mais nas últimas 24h? Escreva SIM ou NÃO: ').lower().strip()[0]
+            if sono == 's':
+                documento = input('\nVocê está com seus documentos em mãos? Escreva SIM ou NÃO: ').lower().strip()[0]
+                if documento == 's':
+                    print ('\nParabéns, você atendeu todos os 5 requisitos para doação!')
+                else:
+                    print('\nNão pode doar porque não está com os documentos!')
+            else:
+                print('\nNão pode doar pelo sono!')
         else:
-            sono = input('Você dormiu 6h nas últimas 24h? Escreva SIM ou NÃO: ').lower().strip()[0]
-            #continuar daqui!
+            print('\nNão pode doar pelo álcool!')
     else:
-        print('Não pode doar')
+        print('\nNão pode doar pelo peso!')
 else:
-    print('Não pode doar')
+    print('\nNão pode doar pela idade!')
