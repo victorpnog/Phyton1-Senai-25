1bcfad7a9d6a53d365769305cae7bee25a030329
Victor Pimentel Nogueira <107135601+victorpnog@users.noreply.github.com>
2025-02-07 21:48:17 -0300

    Add files via upload

--- 025.py
+++ 025.py
@@ -9,11 +9,11 @@ pc = random.randint(1,3)
 print(f'\nA escolha do computador foi: {pc}')
 
 #processamento batalha:
-if jo == pc:
+if (jo == pc):
     print('\nEmpate!')
-elif jo == 1 and pc == 2 or jo == 2 and pc == 3 or jo == 3 and pc == 1:
+elif (jo == 1 and pc == 2) or (jo == 2 and pc) == 3 or (jo == 3 and pc == 1):
     print('\nComputador ganha!')
-elif jo == 1 and pc == 3 or jo == 2 and pc == 1 or jo == 3 and pc == 2:
+elif (jo == 1 and pc == 3) or (jo == 2 and pc) == 1 or (jo == 3 and pc == 2):
     print('\nJogador ganha!')
 else:
     print('\nVocê deve ter jogado outro número além de 1/2/3... Sua mula!!')
